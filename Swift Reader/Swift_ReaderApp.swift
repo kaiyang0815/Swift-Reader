583dbaa16908654b95753c360687f6d725b9dedd
kaiyang0815 <wangkaiyang0815@gmail.com>
2024-10-16 22:50:02 +0800

    Initial Commit

---
+++ Swift Reader/Swift_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ReaderApp.swift
+//  Swift Reader
+//
+//  Created by kaiyang on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift ReaderTests/Swift_ReaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_ReaderTests.swift
+//  Swift ReaderTests
+//
+//  Created by kaiyang on 2024/10/16.
+//
+
+import Testing
+
+struct Swift_ReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
